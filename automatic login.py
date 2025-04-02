dadb2daee4383716be646e0268509b550edb305e
Enilmalus <enil12408@gmail.com>
2025-04-02 08:53:55 +0800

    更新最新版本

--- automatic login.py
+++ automatic login.py
@@ -17,8 +17,8 @@ headers = {
 
 
 data = {
-    'userId': 'BDT23050', # 这里输入学号
-    'password': 'LLzz0223', # 这里输入密码
+    'userId': '', # 这里输入学号
+    'password': '', # 这里输入密码
     'service': '%E4%B8%AD%E5%9B%BD%E7%94%B5%E4%BF%A1',
     'queryString': 'wlanuserip%3D852741297bb41a1b390659a3b6044f4c%26wlanacname%3Da57672291393aa19%26ssid%3D%26nasip%3Dbda1d0b1a7e3370d3b3a8a85599b4300%26snmpagentip%3D%26mac%3Db2fcfcfde2a45affbdd40517301bbd25%26t%3Dwireless-v2%26url%3D2c0328164651e2b4f13b933ddf36628bea622dedcc302b30%26apmac%3D%26nasid%3Da57672291393aa19%26vid%3D30953b9cab08e6f7%26port%3D8bd1bb9d7a29cbc6%26nasportid%3D5b9da5b08a53a5403369c3a2784609dbc8ad14ec61bcfd09890945fb4b96e7ac7c8dda32ebafbb46',
     'operatorPwd': '',
